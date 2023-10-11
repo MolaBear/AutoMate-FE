9be376741cf2c7d022da0e5b5dc471eb2b7f8285
M-Mola <MMola@hollywoodbets.net>
2023-10-11 09:41:54 +0200

    Login connected to API

--- src/Views/AdminDash/SessionsTable/SessionHistory/SessionHistory.tsx
+++ src/Views/AdminDash/SessionsTable/SessionHistory/SessionHistory.tsx
@@ -1,21 +1,23 @@
-
 import React, { useState } from 'react';
 import { useTable, Column } from 'react-table';
-import { Card, SessionsTable } from '../../../../Components/Styled Components/AppStyle';
+import { Card, InputField1, SessionsTable, TableHeader } from '../../../../Components/Styled Components/AppStyle';
+import PopUp from '../../../../Components/Popup';
+import { XMarkIcon } from '@heroicons/react/24/outline';
+import SessionRegister from './Register';
 // import { Router } from 'react-router-dom';
 
 interface SessionData {
   session_name: string
   date: string;
   branch: string;
-  view: string;
+  // view: string;
   // Add more fields...
 }
 
 const sessionData: SessionData[] = [
-  { session_name: 'Powerskills', date: '2023-08-01', branch: 'DBN', view:  'View Session'},
-  { session_name: 'FICA', date: '2023-08-02', branch: 'JHB', view: 'View Session' },
-  { session_name: 'Powerskills', date: '2023-08-03', branch: 'DBN', view: 'View Session' },
+  { session_name: 'Powerskills', date: '2023-08-01', branch: 'DBN'},
+  { session_name: 'FICA', date: '2023-08-02', branch: 'JHB' },
+  { session_name: 'Powerskills', date: '2023-08-03', branch: 'DBN'},
   
   // Add more data...
 ];
@@ -24,12 +26,12 @@ const sessionData: SessionData[] = [
   { Header: 'SESSION NAME', accessor: 'session_name' },
    { Header: 'DATE', accessor: 'date' },
    { Header: 'BRANCH', accessor: 'branch' },
-   { 
-    Header: 'VIEW', 
-    Cell: e =>
+  //  { 
+  //   Header: 'VIEW', 
+  //   Cell: e =>
     
-    <a href={e.value}> {e.value} </a>,
-   accessor: 'view' }, 
+  //   <a href={e.value}> {e.value} </a>,
+  //  accessor: 'view' }, 
    
  ]; 
 
@@ -37,7 +39,7 @@ const sessionData: SessionData[] = [
 
 const SessionHistoryTable: React.FC = () => {
   const [searchDate, setSearchDate] = useState('');
-
+  const [isPopUpOpen, setPopUpOpen] = useState(false);
   
   const {
     getTableProps,
@@ -57,17 +59,28 @@ const SessionHistoryTable: React.FC = () => {
 
 );
 
+const handlePencilIconClick = () => {
+  setPopUpOpen(true);
+};
+
+const handleClosePopUp = () => {
+  setPopUpOpen(false);
+};
+
   
 
   return (
     <div style={{marginLeft: '250px'}} >
       <Card>
-        <h1>All My Sessions</h1>
-          <input
+        <h2>All My Sessions</h2>
+          <InputField1
+            width='18%'
+            fontSize='15px'
             type="date"
             placeholder="Search by date..."
             value={searchDate}
             onChange={(e) => setSearchDate(e.target.value)}
+            style={{margin: '10px', marginInline: "72%"}}
           />
           <SessionsTable {...getTableProps()} className="table">
             <thead>
@@ -76,6 +89,7 @@ const SessionHistoryTable: React.FC = () => {
                   {headerGroup.headers.map(column => (
                     <th {...column.getHeaderProps()}>{column.render('Header')}</th>
                   ))}
+                <TableHeader></TableHeader>
                 </tr>
               ))}
             </thead>
@@ -87,6 +101,21 @@ const SessionHistoryTable: React.FC = () => {
                     {row.cells.map(cell => {
                       return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
                     })}
+                    <td>
+                        <button
+                            className="h-6 w-6 text-gray-500 hover:text-purple-500 cursor-pointer" 
+                            onClick={handlePencilIconClick}
+                            style={{width: "fit-content"}}
+                        > View Session</button>
+                        <PopUp isOpen={isPopUpOpen} onClose={handleClosePopUp}>
+                          <div style={{ display: 'flex', justifyContent: 'right', alignItems: 'center' }}>
+                            <button onClick={handleClosePopUp}>
+                              <XMarkIcon className="h-6 w-6 text-gray-500" />
+                            </button>
+                          </div>
+                          <SessionRegister/>
+                        </PopUp>
+                    </td>
                   </tr>
                 );
               })}
--- src/Views/Login/index.tsx
+++ src/Views/Login/index.tsx
@@ -1,140 +1,229 @@
+import axios from 'axios';
 import {
-    Button,
-    Checkbox,
-    FormControl,
-    FormControlLabel,
-    InputLabel,
-    MenuItem,
-    Select,
-    TextField,
-  } from "@mui/material";
-  import { useNavigate } from "react-router-dom";
-  import { useEffect, useRef, useState } from "react";
-import React from "react";
-import Popup from "reactjs-popup";
-import { InputField1, UserRoleSelect } from "../../Components/Styled Components/AppStyle";
+  Button,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+// import jwt from 'jsonwebtoken';
+
+const API_URL = 'https://localhost:7184/api/User/LoginUser'; 
+const Login: React.FC = () => {
   
+  const [empID, setEmpID] = useState("");
+  const [pass, setPass] = useState("");
+  const navigate = useNavigate();
 
-  const Login = () => {
-    const [empID, setEmpID] = useState("");
-    const [pass, setPass] = useState("");
-    const [securityQ, setSecurityQ] = useState("");
-    const [securityAns, setSecurityAns] = useState("");
-    const navigate = useNavigate();
+  const loginUser = async () => {
+    try {
+      const response = await axios.post(API_URL, {
+        EmployeeNumber: empID,
+        Password: pass,
+      });
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(empID);
-  }
+      const { message, isSucess } = response.data;
 
-    const imgRef = useRef<HTMLImageElement>(null);
-
-    useEffect(() => {
-      if (imgRef.current) {
-        const img = imgRef.current;
-        img.setAttribute('width', img.width.toString());
-        img.setAttribute('height', img.height.toString());
-      }
-    }, []);
-  
-    //Validation (Trainer, Trainee, Admin)
-    const loginUser = () => {
-      if (empID === "Trainee" && pass === "1234") {
-        navigate("setpass");
-      } else if (empID === "Trainer" && pass === "0000") {
-        console.log("trainer in, take me to home");
-        navigate("trainer/home");
-      } else if (empID === "Admin" && pass === "1111") {
-        navigate("admin/home");
-        console.log("admin clocking in, take me to home");
+      if (isSucess) {
+        const { jwtTokenKey } = message;
+        // const decondedToken = jwt.decode(jwtTokenKey);
+        localStorage.setItem('jwtToken', jwtTokenKey);
+        navigate('/trainee/home'); 
       } else {
-        console.log("invalid user");
+        console.log('Invalid credentials');
       }
-    };
-  
-    return (
-      <>
-        {/* <img src={bgLogin} className="login-bg" alt="background image" />  */}
-        <div className="split left">
-          <label className="dvd">DVD</label>
-        </div>
-  
-        <div className="split right">
-          <form className="login-form">
-            <img 
-             ref={imgRef}
-             src="/Logo.gif"
-             className="appLogo"
-             alt="logo"
-                />
-  
-            <TextField
-              className="input-field"
-              label="Employee Code"
-              color="secondary"
-              value={empID}
-              onChange={(e) => setEmpID(e.target.value)}
-              placeholder="BT0000"
-              variant="outlined"
-            />
-  
-            <TextField
-              className="input-field"
-              label="Password"
-              color="secondary"
-              value={pass}
-              onChange={(e) => setPass(e.target.value)}
-              type="password"
-              variant="outlined"
-            />
-  
-            {/* <div className="form-labels">
-              <FormControlLabel
-                control={<Checkbox color="secondary" />}
-                label="Remember Me"
-                color="secondary"
-              />
-            </div> */}
-  
-            <Button 
-              style={{marginBottom: "25px"}} 
-              id="submitBtn" 
-              color="primary" 
-              onClick={loginUser}
-            >
-              Submit
-            </Button>
-
-            
-            <Popup trigger={<label id="forgotPass" style={{marginLeft: "60%", marginTop:"10px"}}>forgot password</label>} modal nested>
-              <div className='modal'>
-                <div className='content'>
-                  <form onSubmit={handleSubmit} className="forgetForm">
-                    <h2>Forgot Password</h2>
-                    <label>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </label>
-                    <br/><br/>
-                    <InputField1 value={empID} onChange={(e) => setEmpID(e.target.value)} type="text" placeholder="Employee ID" id="empID" name="empID" width="100%" />
-                    <br/>
-                    <UserRoleSelect value={securityQ} onChange={(e) => setSecurityQ(e.target.value)} name="SecurityQ" id="SecurityQ" width="100%">
-                      <option value="" disabled>Security Question</option>
-                      <option value="Q1">Question 1</option>
-                      <option value="Q2">Question 2</option>
-                      <option value="Q3">Question 3</option>
-                      <option value="Q4">Question 4</option>
-                    </UserRoleSelect>
-                    <InputField1 value={securityAns} onChange={(e) => setSecurityAns(e.target.value)} type="text" placeholder="Security Question Answer" id="securityAns" name="securityAns" width="100%"/>
-                    <button type="submit" id="submitBtn">Reset Password</button> 
-                  </form> 
-                </div>
-              </div>
-            </Popup>
-          </form>
-        </div>
-      </>
-    );
+    } catch (error) {
+      console.error('Login error:', error);
+    }
   };
+
+  return (
+    <>
+      <div className="split left">
+        <label className="dvd">DVD</label>
+      </div>
+
+      <div className="split right">
+        <form className="login-form">
+            {/* <img 
+               ref={imgRef}
+               src="/Logo.gif"
+               className="appLogo"
+               alt="logo"
+                  /> */}
+          <TextField
+            className="input-field"
+            label="Employee Code"
+            color="secondary"
+            value={empID}
+            onChange={(e) => setEmpID(e.target.value)}
+            placeholder="BT0000"
+            variant="outlined"
+          />
+
+          <TextField
+            className="input-field"
+            label="Password"
+            color="secondary"
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+            type="password"
+            variant="outlined"
+          />
+
+          <Button
+            style={{ marginBottom: "25px" }}
+            id="submitBtn"
+            color="primary"
+            onClick={loginUser}
+          >
+            Submit
+          </Button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
+
+  //  import {
+  //     Button,
+  //     Checkbox,
+  //     FormControl,
+  //     FormControlLabel,
+  //     InputLabel,
+  //     MenuItem,
+  //     Select,
+  //     TextField,
+  //   } from "@mui/material";
+  //   import { useNavigate } from "react-router-dom";
+  //   import { useEffect, useRef, useState } from "react";
+  // import React from "react";
+  // import Popup from "reactjs-popup";
+  // import { InputField1, UserRoleSelect } from "../../Components/Styled Components/AppStyle";
+    
   
-  export default Login;
+  //   const Login = () => {
+  //     const [empID, setEmpID] = useState("");
+  //     const [forgotPassID, setForgotPassID] = useState("");
+  //     const [email, setEmail] = useState("");
+  //     const [pass, setPass] = useState("");
+  //     const [securityQ, setSecurityQ] = useState("");
+  //     const [securityAns, setSecurityAns] = useState("");
+  //     const navigate = useNavigate();
   
+  //     const handleSubmit = (e) => {
+  //       e.preventDefault();
+  //       console.log(empID);
+  //   }
+  
+  //     const imgRef = useRef<HTMLImageElement>(null);
+  
+  //     useEffect(() => {
+  //       if (imgRef.current) {
+  //         const img = imgRef.current;
+  //         img.setAttribute('width', img.width.toString());
+  //         img.setAttribute('height', img.height.toString());
+  //       }
+  //     }, []);
+    
+  //     //Validation (Trainer, Trainee, Admin)
+  //     const loginUser = () => {
+  //       if (empID === "Trainee" && pass === "1234") {
+  //         navigate("setpass");
+  //       } else if (empID === "Trainee" && pass === "Password123!") {
+  //         console.log("trainee in, take me to home");
+  //         window.location.replace("trainee");
+  //       }else if (empID === "Trainer" && pass === "0000") {
+  //         console.log("trainer in, take me to home");
+  //         navigate("trainer/sessions/view");
+  //       } else if (empID === "Admin" && pass === "1111") {
+  //         navigate("admin/sessions/view");
+  //         console.log("admin clocking in, take me to home");
+  //       } else {
+  //         console.log("invalid user");
+  //       }
+  //     };
+    
+  //     return (
+  //       <>
+  //         {/* <img src={bgLogin} className="login-bg" alt="background image" />  */}
+  //         <div className="split left">
+  //           <label className="dvd">DVD</label>
+  //         </div>
+    
+  //         <div className="split right">
+  //           <form className="login-form">
+
+    
+  //             <TextField
+  //               className="input-field"
+  //               label="Employee Code"
+  //               color="secondary"
+  //               value={empID}
+  //               onChange={(e) => setEmpID(e.target.value)}
+  //               placeholder="BT0000"
+  //               variant="outlined"
+  //             />
+    
+  //             <TextField
+  //               className="input-field"
+  //               label="Password"
+  //               color="secondary"
+  //               value={pass}
+  //               onChange={(e) => setPass(e.target.value)}
+  //               type="password"
+  //               variant="outlined"
+  //             />
+    
+  //             {/* <div className="form-labels">
+  //               <FormControlLabel
+  //                 control={<Checkbox color="secondary" />}
+  //                 label="Remember Me"
+  //                 color="secondary"
+  //               />
+  //             </div> */}
+    
+  //             <Button 
+  //               style={{marginBottom: "25px"}} 
+  //               id="submitBtn" 
+  //               color="primary" 
+  //               onClick={loginUser}
+  //             >
+  //               Submit
+  //             </Button>
+  
+              
+  //             <Popup trigger={<label id="forgotPass" style={{marginLeft: "60%", marginTop:"10px"}}>forgot password</label>} modal nested>
+  //               <div className='modal'>
+  //                 <div className='content'>
+  //                   <form onSubmit={handleSubmit} className="forgetForm">
+  //                     <h2>Forgot Password</h2>
+  //                     <label>
+  //                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  //                     </label>
+  //                     <br/><br/>
+  //                     <InputField1 value={forgotPassID} onChange={(e) => setEmpID(e.target.value)} type="text" placeholder="Employee Code" id="empID" name="empID" width="100%" /><br/>
+  //                     <InputField1 value={email} onChange={(e) => setEmpID(e.target.value)} type="text" placeholder="Email Address" id="email" name="email" width="100%" />
+  //                     <br/>
+  //                     <UserRoleSelect value={securityQ} onChange={(e) => setSecurityQ(e.target.value)} name="SecurityQ" id="SecurityQ" width="100%">
+  //                       <option value="" disabled>Security Question</option>
+  //                       <option value="Q1">Question 1</option>
+  //                       <option value="Q2">Question 2</option>
+  //                       <option value="Q3">Question 3</option>
+  //                       <option value="Q4">Question 4</option>
+  //                     </UserRoleSelect>
+  //                     <InputField1 value={securityAns} onChange={(e) => setSecurityAns(e.target.value)} type="text" placeholder="Security Question Answer" id="securityAns" name="securityAns" width="100%"/>
+  //                     <button type="submit" id="submitBtn">Reset Password</button> 
+  //                   </form> 
+  //                 </div>
+  //               </div>
+  //             </Popup>
+  //           </form>
+  //         </div>
+  //       </>
+  //     );
+  //   };
+    
+  //   export default Login;
+    
